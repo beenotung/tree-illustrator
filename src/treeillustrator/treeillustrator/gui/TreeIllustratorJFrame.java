5b3b32a26a892658f848c1c45734119f9324eaaf
Beeno Tung <aabbcc1241@yahoo.com.hk>
2014-12-08 14:45:29 +0800

    updated CanvasJFrame

--- src/treeillustrator/treeillustrator/gui/TreeIllustratorJFrame.java
+++ src/treeillustrator/treeillustrator/gui/TreeIllustratorJFrame.java
@@ -1,9 +1,48 @@
 package treeillustrator.gui;
 
-import javax.swing.*;
+import myutils.gui.CanvasJFrame;
+import treeillustrator.StaticStuff;
 
 /**
  * Created by beenotung on 12/7/14.
  */
-public class TreeIllustratorJFrame extends JFrame {
+public class TreeIllustratorJFrame extends CanvasJFrame {
+
+    public TreeIllustratorJFrame(double widthRate, double heightRate, int scale) {
+        super((int) Math.round(screenSize.getWidth() * widthRate), (int) Math.round(screenSize.getHeight() * heightRate), scale, StaticStuff.APP_NAME, DEFAULT_NS_PER_TICK, DEFAULT_NS_PER_RENDER);
+    }
+
+    public TreeIllustratorJFrame() {
+        this(1d, 1d, 1);
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void myTick() {
+
+    }
+
+    @Override
+    protected void myRender() {
+
+    }
+
+    @Override
+    protected void myDebugInfo() {
+
+    }
+
+    @Override
+    protected void myKeyHandling() {
+
+    }
+
+    @Override
+    protected void myMouseHandling() {
+
+    }
 }
